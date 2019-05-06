b6af9560737b0fb87f55ddc908a4b8f7f58428b8
nathanmailhiot <nathanjm12@outlook.com>
2019-05-06 17:39:10 -0400

    Add events, api, ui, app

--- assets/scripts/books/ui.js
+++ assets/scripts/books/ui.js
@@ -1,25 +1,24 @@
 const showItemsTemplate = require('./../templates/helpers/item-listing.handlebars')
 
-const onGetBooksSuccess = funciton (data) {
+const onGetBooksSuccess = function (data) {
   const showItemsHtml = showItemsTemplate({ books: data.books })
-$('.content').html(showItemsHtml)
-if (data.books.length === 0) {
-  $('.messaging').text('Add an item to the list!')
-  setTimeout(function () {
-    $('.messaging').text('')
+  $('.content').html(showItemsHtml)
+  if (data.books.length === 0) {
+    $('.messaging').text('Add an item to the list!')
+    setTimeout(function () {
+      $('.messaging').text('')
     }, 2000)
   }
 }
-
 
 const onGetBooksFailure = function (data) {
   $('.messaging').text('You were unable to find any books')
   setTimeout(function () {
     $('.messaging').text('')
-    }, 2000)
+  }, 2000)
 }
 
 module.exports = {
-onGetBooksSuccess,
-onGetBooksFailure
+  onGetBooksSuccess,
+  onGetBooksFailure
 }
